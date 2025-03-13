59ecf2013c4afbc3ae6dc732c076f8de769c8863
noook <noook@yandex.ru>
2025-03-13 17:22:26 +0700

    #2

--- main.py
+++ main.py
@@ -14,8 +14,24 @@ with open(file_csv, encoding='cp1251', newline='') as csvfile:
 config = fabric.Config(overrides={"sudo": {"password": lu_conf.secret}})
 
 for comp in comp_dict:
-    print(comp, end='   = ')
-    conn = fabric.Connection(host=comp, user=lu_conf.user, connect_kwargs={"password": lu_conf.secret}, config=config)
+    print(comp, end=' = ')
+    conn = fabric.Connection(host=comp, user=lu_conf.user, connect_kwargs={"password": lu_conf.secret},
+                             config=config, connect_timeout=5)
+    print(conn.is_connected)
     comp_dict[comp] = conn.run('uname -r')
-    conn.close()
+
+    # try:
+    #     conn = fabric.Connection(host=comp, user=lu_conf.user, connect_kwargs={"password": lu_conf.secret},
+    #                              config=config, connect_timeout=5)
+    #     print(conn.is_connected.bit_count())
+    # # except TimeoutError as _err:
+    # #     print('какая-то ошибка', _err)
+    # except Exception as _err:
+    #     print('какая-то ошибка', _err)
+    # else:
+    #     print(conn.is_connected.bit_count())
+    #     # comp_dict[comp] = conn.run('uname -r')
+    #     # conn.close()
+
     print()
+    conn.close()
