7d620d17d105ae171bed71e46e96b1a158f4918c
noook <noook@yandex.ru>
2025-03-24 09:58:18 +0700

    #5 добавил каменты

--- main.py
+++ main.py
@@ -13,8 +13,10 @@ with open(file_csv, encoding='cp1251', newline='') as csvfile:
     for row in row_csv_content:
         comp_dict[row[0]] = ''
 
+# общий конфиг для всех соединений
 config = fabric.Config(overrides={"sudo": {"password": lu_conf.secret}})
 
+# цикл подключения ко всем компам из списка в файле
 for comp in comp_dict:
     print(comp, end=' = ')
     conn = fabric.Connection(host=comp, user=lu_conf.user, connect_kwargs={"password": lu_conf.secret},
@@ -25,16 +27,6 @@ for comp in comp_dict:
             print(f'... {i} ... {getattr(conn, i, None)}')
             print('_' * 45)
     print()
-
-
-
-
-
-
-
-
-
-
 
     # print()
     # conn.close()
