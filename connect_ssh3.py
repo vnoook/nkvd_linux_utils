0dfe669d43a4287f9870277c01bd352757553a9d
noook <noook@yandex.ru>
2026-08-17 11:53:28 +0700

    #80 edit connect_ssh3.py

--- connect_ssh3.py
+++ connect_ssh3.py
@@ -2,12 +2,25 @@ import concurrent.futures
 import paramiko
 import csv
 import lu_conf  # файл с доступами
+import connect_ssh2
 
 # Настройки подключения
 SSH_USER = lu_conf.user
 SSH_PASSWORD = lu_conf.secret
 TARGET_DIR = lu_conf.users_dir
-file_csv = 'hosts.csv'
+file_csv = 'res/hosts.csv'
+users_csv = 'res/users.csv'
+
+
+# функция чтения файла и получения из него списка имён пользователей и их секреты
+def read_users_csv(users_file) -> dict:
+    users_dict = dict()
+    # чтение файла с пользователями
+    with open(users_file, encoding='cp1251', newline='') as csvfile:
+        row_csv_content = csv.reader(csvfile, delimiter=',')
+        for row in row_csv_content:
+            users_dict[row[0]] = row[1]
+    return users_dict
 
 
 # функция чтения файла и получения из него списка имён компов
@@ -32,7 +45,7 @@ def get_folders_from_host(host):
             hostname=host,
             username=SSH_USER,
             password=SSH_PASSWORD,
-            timeout=5,
+            timeout=3,
         )
 
         # Команда выводит только имена папок (тип d) в указанной директории
@@ -60,21 +73,24 @@ def main():
     print("Начинаю сбор данных с компьютеров...\n")
 
     comp_list = read_file_csv(file_csv)
+    users_dict = read_users_csv(users_csv)
 
     # Запуск опроса в несколько потоков для скорости
     with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
         results = executor.map(get_folders_from_host, comp_list)
 
-    # Вывод результатов
     for host, output in results:
-        print(f"=== Результат для {host} ===")
+        print("=== Результат для " + "\033[1;32m" + f"{host}" + "\033[0m" + " ===")
         if isinstance(output, list):
             for folder in output:
-                print(f"  [Папка] {folder}")
+                user_name = folder
+                if user_name in users_dict:
+                    print(user_name)
+                    # print(f"  [Папка] {user_name}")
+                    # print(f"    [Пользователь] {user_name}")
         else:
             print(f"  {output}")
         print("-" * 40)
-
 
 if __name__ == "__main__":
     main()
